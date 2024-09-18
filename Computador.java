b10ecb0591dae394276f28612504c7adde9b4078
amarallorena <159277845+amarallorena@users.noreply.github.com>
2024-09-18 15:35:18 -0300

    computador

---
+++ Computador.java
@@ -0,0 +1,61 @@
+package Computador;
+
+	public class Computador{
+		String modelo;
+		double custo;
+		String cor;
+		int placas;
+		String processador;
+		String memória;
+		String Videoplaca;
+		String cooler;
+		public String getModelo() {
+		return modelo;
+		}
+		public void setModelo(String modelo) {
+		this.modelo = modelo;
+		}
+		public double getcusto() {
+		return custo;
+		}
+		public void setcusto(double custo) {
+		this.custo = custo;
+		}
+		public String getCor() {
+		return cor;
+		}
+		public void setCor(String cor) {
+		this.cor = cor;
+		}
+		public int getPlacas() {
+		return placas;
+		}
+		public void setPlacas(int placas) {
+		this.placas = placas;
+		}
+		public String getprocessador() {
+		return processador;
+		}
+		public void setprocessador(String processador) {
+		this.processador = processador;
+		}
+		public String getmemória() {
+		return memória;
+		}
+		public void setmemória(String memória) {
+		this.memória = memória;
+		}
+		public String getVideoplaca() {
+		return Videoplaca;
+		}
+		public void setVideoplaca(String Videoplaca) {
+		this.Videoplaca = Videoplaca;
+		}
+		public String getcooler() {
+		return cooler;
+		}
+		public void setcooler(String cooler) {
+		this.cooler = cooler;
+	}
+
+}
